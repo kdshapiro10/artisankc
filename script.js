f36a35c2063e98492a7ac0f4bace1c580134893c
Kylie Shapiro <kdshapir@asu.edu>
2024-10-25 20:21:36 -0700

    css

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const navEl = document.querySelector(".nav");
 const hamburgerEl = document.querySelector(".hamburger");
-const mainEl = document.getElementById("main-info");
+const mainInfo = document.getElementById("main-info");
 
 hamburgerEl.addEventListener("click", () => {
     navEl.classList.toggle("nav--open");
     hamburgerEl.classList.toggle("hamburger--open");
-    mainEl.classList.add("main-info");
+    mainInfo.classList.add("main-info");
 });
 
 navEl.addEventListener("click", () => {
     navEl.classList.remove("nav--open");
     hamburgerEl.classList.remove("hamburger--open");
-    mainEl.classList.remove("main-info");
+    mainInfo.classList.remove("main-info");
 });
