cf957bd403b7111e7c98c8b60133403b471d4e9d
Kylie Shapiro <kdshapir@asu.edu>
2024-10-25 20:05:15 -0700

    index.html

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-/* FORM STYLES */
+const navEl = document.querySelector(".nav");
+const hamburgerEl = document.querySelector(".hamburger");
+const mainEl = document.getElementById("main-info");
+
+hamburgerEl.addEventListener("click", () => {
+    navEl.classList.toggle("nav--open");
+    hamburgerEl.classList.toggle("hamburger--open");
+    mainEl.classList.add("main-info");
+});
+
+navEl.addEventListener("click", () => {
+    navEl.classList.remove("nav--open");
+    hamburgerEl.classList.remove("hamburger--open");
+    mainEl.classList.remove("main-info");
+});
