4564e4995f9eaac79ff4d84d6bc376483307085a
Kylie Shapiro <kdshapir@asu.edu>
2024-11-10 18:16:36 -0700

    about

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const navEl = document.querySelector(".nav");
 const hamburgerEl = document.querySelector(".hamburger");
 const mainInfo = document.getElementById("main-info");
 
+
 hamburgerEl.addEventListener("click", () => {
     navEl.classList.toggle("nav--open");
     hamburgerEl.classList.toggle("hamburger--open");
     mainInfo.classList.add("main-info");
+
 });
 
 navEl.addEventListener("click", () => {
     navEl.classList.remove("nav--open");
     hamburgerEl.classList.remove("hamburger--open");
     mainInfo.classList.remove("main-info");
+   
 });
