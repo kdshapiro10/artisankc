73761d24f324a8b9e86c99a5d9e9007489d84065
Kylie Shapiro <kdshapir@asu.edu>
2024-11-12 17:40:58 -0700

    index.html
    deleted button element replaced with just link

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const mainInfo = document.getElementById("main-info");
 
 
 hamburgerEl.addEventListener("click", () => {
-    navEl.classList.toggle(".nav--open");
+    navEl.classList.toggle("nav--open");
     hamburgerEl.classList.toggle("hamburger--open");
     mainInfo.classList.add("main-info");
 
 });
 
 navEl.addEventListener("click", () => {
-    navEl.classList.remove(".nav--open");
+    navEl.classList.remove("nav--open");
     hamburgerEl.classList.remove("hamburger--open");
     mainInfo.classList.remove("main-info");
    
